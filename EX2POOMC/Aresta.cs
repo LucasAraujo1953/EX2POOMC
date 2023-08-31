77c6d887799547769117ce5abef7b992a2c70003
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-31 18:28:42 -0300

    Add files via upload

---
+++ EX2POOMC/Aresta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2POOMC
+{
+    internal class Aresta
+    {
+        private double aresta;
+
+
+
+        public Aresta(double aresta)
+
+        {
+
+            this.aresta = aresta;
+
+        }
+
+
+
+        public double CalcularArea()
+
+        {
+
+            return aresta *aresta;
+
+        }
+    }
+}
